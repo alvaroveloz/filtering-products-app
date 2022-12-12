7644efcc96f5b9c1a949cbbaa8a8b4a3353847f6
alvaroveloz <alvaro.veloz@gmail.com>
2022-12-11 21:53:24 -0500

    CHALLENGE - Loading products

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,38 @@
+import { GetStaticProps } from 'next'
 import styles from '../styles/Home.module.css'
+import { Product } from './types/Product';
+import getProducts from './api/getProducts';
+import ProductCard from './components/ProductCard';
 
-export default function Home() {
+type Props = {
+  products: Product[];
+}
+
+const Home = ( { products }: Props ) => {
+  
   return (
-   <div>Filtering Products App</div>
+    <main>
+      <aside><h1>Filtering Products App</h1></aside>
+      <section>
+          { products.map((product)=> (
+        <article key={product.id}>
+            <ProductCard product={product}></ProductCard>
+        </article>
+          ) ) }
+
+      </section>
+    </main>
   )
 }
+
+export const getStaticProps: GetStaticProps<Props> = async () => {
+  const products = await getProducts();
+
+  return {
+    props: {
+      products,
+    }
+  }
+}
+
+export default Home;

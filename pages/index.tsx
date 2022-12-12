5b826227a6c991d08f57f067caefb0b1b2cdc684
alvaroveloz <alvaro.veloz@gmail.com>
2022-12-12 13:20:20 -0500

    CHALLENGE - Adding aside components

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,19 +1,33 @@
-import { GetStaticProps } from 'next'
-import styles from '../styles/Home.module.css'
+import { GetStaticProps, NextPage } from 'next'
 import { Product } from './types/Product';
 import getProducts from './api/getProducts';
 import ProductCard from './components/ProductCard';
+import PriceRangeFilter from './components/PriceRangeFilter';
+import ColorFilter from './components/ColorFilter';
+import RatingFilter from './components/RatingFilter';
+import { useState } from 'react';
 
 type Props = {
   products: Product[];
 }
 
-const Home = ( { products }: Props ) => {
+const Home: NextPage<Props> = ( { products }: Props ) => {
   
+  const [filters, setFilters] = useState({
+    price: null,
+    color: null,
+    rating: null,
+  })
+
   return (
-    <main>
-      <aside><h1>Filtering Products App</h1></aside>
-      <section>
+    <main style={{display: 'flex'}}>
+      <aside>
+        <h1>Filtering Products App</h1>
+        <PriceRangeFilter />
+        <ColorFilter products={ products } />
+        <RatingFilter />
+        </aside>
+      <section style={{ flex: 1, display: 'grid', gridTemplateColumns: 'repeat(auto-fill, minmax(300px, 1fr))', gap: 12 }}>
           { products.map((product)=> (
         <article key={product.id}>
             <ProductCard product={product}></ProductCard>

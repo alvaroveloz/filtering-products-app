19e8583e4475fe9227e5b296b939f7f6a2fe4623
alvaroveloz <alvaro.veloz@gmail.com>
2022-12-12 13:20:44 -0500

    CHALLENGE -  Limit results in fetch

--- pages/api/getProducts.ts
+++ pages/api/getProducts.ts
@@ -1,7 +1,9 @@
 import { Product } from "../types/Product";
 
 const getProducts = async (): Promise<Product[]> => {
-    const response = await fetch('http://api.devtoolstech.in/ecommerce/products');
+    const response = await fetch(
+      'http://api.devtoolstech.in/ecommerce/products?_limit=20'
+    );
     const products = await response.json();
     return  products; 
 

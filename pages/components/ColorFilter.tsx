6199f212a6362849c68db012dd5b9fefbdd1c0f7
alvaroveloz <alvaro.veloz@gmail.com>
2022-12-12 21:56:35 -0500

    CHALLENGE -  Added handleChange in ColorFilter

--- pages/components/ColorFilter.tsx
+++ pages/components/ColorFilter.tsx
@@ -1,38 +1,66 @@
-import React, { useMemo } from "react"
+import React, { useMemo, useState } from 'react';
+import { Filter } from '../types/Filter';
 import { Product } from '../types/Product';
 
 type Props = {
   products: Product[];
+  onChange: (filter: Filter) => void;
 };
 
-const ColorFilter: React.FC<Props> = ({ products }) => {
+const ColorFilter: React.FC<Props> = ({ products, onChange }) => {
+
+  const [selected, setSelected] = useState<Set<string>>(()=> new Set());
+
   const colors = useMemo(() => {
     const buffer: Set<string> = new Set();
 
     for (let product of products) {
       buffer.add(product.color);
-      
     }
     return Array.from(buffer);
-
   }, [products]);
 
-  const handleChange = (value: string, checked: boolean) => {
-    console.log({ value, checked});
-  }
+  const handleChange = (value: string, isChecked: boolean) => {
+    
+    const draft = structuredClone(selected);
+    if (isChecked) {
+      draft.add(value);
+    } else{
+      draft.delete(value);
+    }
 
+    onChange(draft.size ? (product)=> draft.has(product.color) : null );
+
+    setSelected(draft);
+  };
   
+  console.log(selected);
+
   return (
-    <ul style={{border: '1px solid white', padding: 12}}>
-      { colors.map((color)=>(
-        <li key={color}>
-          <input type='checkbox' value={color} onChange={(e)=> handleChange(e.target.value, e.target.checked)} />
-          <label> { color } </label>
+    <div
+      style={{
+        border: '1px solid white',
+        padding: 12,
+        display: 'flex',
+        flexDirection: 'column',
+        gap: 12,
+      }}
+    >
+      <h4>Filter by Colors</h4>
+      <ul style={{ listStyle: 'none' }}>
+        {colors.map((color) => (
+          <li key={color}>
+            <input
+              type='checkbox'
+              value={color}
+              onChange={(e) => handleChange(e.target.value, e.target.checked)}
+            />
+            <label> {color} </label>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
 
-        </li>
-      ))}
-    </ul>
-  )
-}
-
-export default ColorFilter
+export default ColorFilter;
